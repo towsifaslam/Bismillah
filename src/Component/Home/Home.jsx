9f668225bac04af18630740c38e2319346008397
Towsif_aslam <towsifaslam>
2022-10-26 22:57:07 +0600

    initial commit

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "bootstrap";
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <Button variant="primary">Button</Button>
+      </div>
+    </div>
+  );
+};
+export default Home;
